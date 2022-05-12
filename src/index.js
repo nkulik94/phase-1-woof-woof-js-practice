601cc27625a2a18700a61999452d0fe179361747
Naftali Kulik <nkulik1994@gmail.com>
2022-05-11 20:31:56 -0400

    completed bonus requirement and DRYed up PATCH functions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,49 @@
-window.addEventListener('DOMContentLoaded', getDogs)
+const filter = document.getElementById('good-dog-filter')
+window.addEventListener('DOMContentLoaded', () => {
+    checkFilter()
+    getDogs()
+})
+
+function checkFilter() {
+    fetch('http://localhost:3000/filter')
+    .then(res => res.json())
+    .then(data => {
+        data[0].isOn === false ? filter.textContent = 'Filter good dogs: OFF' : filter.textContent = 'Filter good dogs: ON'
+        filter.addEventListener('click', () => updateInfo.call(data[0], 'isOn', 'filter', updateFilter))
+    })
+}
+
+function updateFilter(data) {
+    data['isOn'] === false ? filter.textContent = 'Filter good dogs: OFF' : filter.textContent = 'Filter good dogs: ON'
+    Array.from(document.querySelectorAll('span')).map(dog => dog.remove())
+    getDogs()
+}
+
+function updateInfo(key, resource, fnc) {
+    this[key] === true ? this[key] = false : this[key] = true
+    const configObj = {
+        method: 'PATCH',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(this)
+    }
+    fetch(`http://localhost:3000/${resource}/${this.id}`, configObj)
+    .then(res => res.json())
+    .then(res => fnc(res))
+
+}
 
 function getDogs() {
     fetch('http://localhost:3000/pups')
     .then(res => res.json())
     .then(data => {
-        data.map(dog => addDogToSpan(dog))
+        data.map(dog => {
+            if (filter.textContent === 'Filter good dogs: OFF' || dog.isGoodDog === true) {
+                addDogToSpan(dog)
+            }
+        })
     })
 }
 
@@ -14,6 +53,7 @@ function addDogToSpan(dog) {
     document.getElementById('dog-bar').appendChild(span)
     span.addEventListener('click', () => showDogInfo(dog))
 }
+
 function showDogInfo(dog) {
     let dogStatus
     dog.isGoodDog === true ? dogStatus = 'Good Dog!' : dogStatus = 'Bad Dog!'
@@ -22,24 +62,5 @@ function showDogInfo(dog) {
     <h2>${dog.name}</h2>
     <button id="status-button">${dogStatus}</button>
     `
-    document.querySelector('#status-button').addEventListener('click', () => updateGoodness(dog))
+    document.querySelector('#status-button').addEventListener('click', () => updateInfo.call(dog, 'isGoodDog', 'pups', showDogInfo))
 }
-
-function updateGoodness(dog) {
-    dog.isGoodDog === true ? dog.isGoodDog = false : dog.isGoodDog = true
-    const configObj = {
-        method: 'PATCH',
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        },
-        body: JSON.stringify(dog)
-    }
-    fetch(`http://localhost:3000/pups/${dog.id}`, configObj)
-    .then(res => res.json())
-    .then(dog => showDogInfo(dog))
-}
-
-document.getElementById('good-dog-filter').addEventListener('click', (e) => {
-    e.target.textContent === 'Filter good dogs: OFF' ? e.target.textContent = 'Filter good dogs: ON' : e.target.textContent = 'Filter good dogs: OFF'
-})
